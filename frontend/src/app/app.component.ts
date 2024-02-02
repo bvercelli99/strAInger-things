dee55bde944cd7323dfbe18b638dd777f633bf96
brianv <brianv@SitesUSA.onmicrosoft.com>
2024-02-02 08:57:11 -0700

    removing hawkins lab for the time being until prompt is finished

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,15 +17,15 @@ import { QueueStatus } from './models/queue-status.enum';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  srcImage: {imageId: string, src: string} | null = null;
+  srcImage: { imageId: string, src: string } | null = null;
   srcLoading = false;
-  aiImage: ImageQueueInfo | null =  null;
+  aiImage: ImageQueueInfo | null = null;
   aiLoadingQuip: number | null = null; //stores the index of quip
   timer: any | undefined;
   readonly destinations = [
-    {name: "Upside Down", value: 1},
-    {name: "Starcourt Mall",  value: 2},
-    {name: "Hawkins Labratory",  value: 3 }
+    { name: "Upside Down", value: 1 },
+    { name: "Starcourt Mall", value: 2 },
+    //{name: "Hawkins Labratory",  value: 3 }
   ];
   readonly quips = [
     "Nancy & Steve are stuck in the Upside Down, there are XXX requests in front of you.",
@@ -39,10 +39,10 @@ export class AppComponent implements OnInit {
     "Hopper just knocked out some Russians, watch out... there are XXX requests in front of you.",
     "Joyce and Hopper entered the Upside Down through the Gate... there are XXX requests in front of you."
   ]
-  
-  @ViewChild("sel") selInput:ElementRef | undefined;
 
-  constructor(private imgService: ImageService){
+  @ViewChild("sel") selInput: ElementRef | undefined;
+
+  constructor(private imgService: ImageService) {
 
   }
 
@@ -57,30 +57,30 @@ export class AppComponent implements OnInit {
     });*/
   }
 
-  canSubmit(): boolean{
-    if(this.srcImage != null && !this.srcLoading && !this.aiLoadingQuip){
+  canSubmit(): boolean {
+    if (this.srcImage != null && !this.srcLoading && !this.aiLoadingQuip) {
       return true;
     }
     return false;
   }
 
-  onFileDropped(file: any){
+  onFileDropped(file: any) {
     console.log('onFileDropped', file[0]);
     this.uploadImage(file[0]);
-    
+
   }
-  fileBrowseHandler(evt: any){
+  fileBrowseHandler(evt: any) {
     console.log('fileBrowseHandler', evt.target.files[0]);
     this.uploadImage(evt.target.files[0]);
   }
   onProcessClick() {
     const promptType = this.selInput?.nativeElement.value as number;
-    if(this.srcImage){
-      
-      this.imgService.addImageToQueue(this.srcImage.imageId, promptType).pipe(take(1)).subscribe({ 
-        next: (placeInLine:{placeInLine: number}) => {
-          try{
-            this.aiLoadingQuip = Math.floor(Math.random() * this.quips.length); 
+    if (this.srcImage) {
+
+      this.imgService.addImageToQueue(this.srcImage.imageId, promptType).pipe(take(1)).subscribe({
+        next: (placeInLine: { placeInLine: number }) => {
+          try {
+            this.aiLoadingQuip = Math.floor(Math.random() * this.quips.length);
             this.aiImage = createImageQueueStatus({
               imageName: this.srcImage?.imageId,
               status: QueueStatus.WAITING,
@@ -89,17 +89,17 @@ export class AppComponent implements OnInit {
 
             this.startImageIntervalCheck(this.aiImage.imageName);
           }
-          catch (e){
+          catch (e) {
             alert('There was an error trying to process your image');
-            
+
           }
         },
         error: (error) => console.log('Error', error)
       });
     }
   }
-  getErrorMessage(status: number): string{
-    switch(status){
+  getErrorMessage(status: number): string {
+    switch (status) {
       case 400:
         return "Something went wrong. The people in your image must be famous, or someone needs to put a shirt on.😬"
       case 429:
@@ -112,34 +112,34 @@ export class AppComponent implements OnInit {
     }
   }
 
-  getQuip(): string{
-    if(this.aiLoadingQuip != null && this.aiImage != null){
-      if(this.aiImage.placeInLine == 0){
+  getQuip(): string {
+    if (this.aiLoadingQuip != null && this.aiImage != null) {
+      if (this.aiImage.placeInLine == 0) {
         return "You're request is processing now hang tight!"
       }
-      else{
-        return this.quips[this.aiLoadingQuip].replace("there are XXX requests in front of you.", 
-        "there " + (this.aiImage.placeInLine > 1 ? "are " : "is ") + this.aiImage.placeInLine + (this.aiImage.placeInLine > 1 ? " requests " : " request ") + " in front of you." );
+      else {
+        return this.quips[this.aiLoadingQuip].replace("there are XXX requests in front of you.",
+          "there " + (this.aiImage.placeInLine > 1 ? "are " : "is ") + this.aiImage.placeInLine + (this.aiImage.placeInLine > 1 ? " requests " : " request ") + " in front of you.");
       }
     }
     return "";
   }
 
-  onDownloadImageClick(){
-    if(this.aiImage != null && this.aiImage.url != ""){
+  onDownloadImageClick() {
+    if (this.aiImage != null && this.aiImage.url != "") {
       window.open(this.aiImage.url, "_blank");
     }
   }
-  onClearSourceClick(){
+  onClearSourceClick() {
     this.srcImage = null;
     this.srcLoading = false;
   }
 
-  private async uploadImage(file: any){
+  private async uploadImage(file: any) {
     this.srcLoading = true;
-    this.imgService.uploadNewImage(file).pipe(take(1)).subscribe({ 
-      next: async (imageId:string) => {
-        try{
+    this.imgService.uploadNewImage(file).pipe(take(1)).subscribe({
+      next: async (imageId: string) => {
+        try {
           const base64 = await this.fileToBase64(file);
           this.srcImage = {
             imageId: imageId,
@@ -148,7 +148,7 @@ export class AppComponent implements OnInit {
           this.srcLoading = false;
           console.log(this.srcImage);
         }
-        catch (e){
+        catch (e) {
           alert('There was an error uploading your image');
           this.srcImage = null;
           this.srcLoading = false;
@@ -158,65 +158,65 @@ export class AppComponent implements OnInit {
     });
   }
 
-  private startImageIntervalCheck(imageName: string){
+  private startImageIntervalCheck(imageName: string) {
     this.timer = window.setInterval(() => {
-      
-        this.imgService.getQueueStatusOfImage(imageName).pipe(take(1)).subscribe({ 
-          next: (info:ImageQueueInfo) => {
-            try{
-              if(this.aiImage != null){
-                switch(info.status){
-                  case QueueStatus.WAITING:
-                  case QueueStatus.PROCESSING:
-                    this.aiImage.placeInLine = info.placeInLine;
-                  break;
-                  case QueueStatus.DONE:
-                    this.aiImage.placeInLine = 0;
-                    this.aiImage.status = info.status;
-                    this.aiImage.url = info.url;
-                    window.clearInterval(this.timer);
 
-                    this.aiLoadingQuip = null;
+      this.imgService.getQueueStatusOfImage(imageName).pipe(take(1)).subscribe({
+        next: (info: ImageQueueInfo) => {
+          try {
+            if (this.aiImage != null) {
+              switch (info.status) {
+                case QueueStatus.WAITING:
+                case QueueStatus.PROCESSING:
+                  this.aiImage.placeInLine = info.placeInLine;
                   break;
-                  case QueueStatus.ERRORED:
-                    this.aiImage.placeInLine = 0;
-                    this.aiImage.status = info.status;
-                    this.aiImage.url = "";
-                    this.aiImage.errorCode = info.errorCode;
-                    window.clearInterval(this.timer);
+                case QueueStatus.DONE:
+                  this.aiImage.placeInLine = 0;
+                  this.aiImage.status = info.status;
+                  this.aiImage.url = info.url;
+                  window.clearInterval(this.timer);
 
-                    this.aiLoadingQuip = null;
+                  this.aiLoadingQuip = null;
                   break;
-                }
+                case QueueStatus.ERRORED:
+                  this.aiImage.placeInLine = 0;
+                  this.aiImage.status = info.status;
+                  this.aiImage.url = "";
+                  this.aiImage.errorCode = info.errorCode;
+                  window.clearInterval(this.timer);
+
+                  this.aiLoadingQuip = null;
+                  break;
               }
             }
-            catch (e){
-              alert('There was an error trying to process your image');
-            }
-          },
-          error: (error) => console.log('Error', error)
-        });
-      
+          }
+          catch (e) {
+            alert('There was an error trying to process your image');
+          }
+        },
+        error: (error) => console.log('Error', error)
+      });
+
     }, 15000)
   }
 
   private async fileToBase64(file: any) {
     return new Promise((resolve, reject) => {
       // Encode the file using the FileReader API
-    const reader = new FileReader();
-    reader.onloadend = () => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
         console.log(reader.result);
         resolve(reader.result as string);
-    };
-    reader.onerror = (e) => {
-      console.log(e);
-      reject(e);
-    }
-    reader.readAsDataURL(file);
+      };
+      reader.onerror = (e) => {
+        console.log(e);
+        reject(e);
+      }
+      reader.readAsDataURL(file);
 
     })
   }
 
-  
+
 
 }
